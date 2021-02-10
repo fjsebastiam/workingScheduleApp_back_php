50ac3acfcca2c8a1a9de2e79b306bd852fae60e7
fjsebastiam <fjsebastiam@gmail.com>
2021-02-10 22:47:02 +0100

    Test logger

--- src/Controller/LibraryController.php
+++ src/Controller/LibraryController.php
@@ -2,16 +2,22 @@
 
 namespace App\Controller;
 
+use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class LibraryController extends AbstractController{
+
+    public function __construct(LoggerInterface $logger){
+        $this->logger = $logger;
+    }
     /**
     * @Route("/library/list")  , name ="library_list"  
     */
     public function list(){
+        $this->logger->info('Libreria');
         $response = new JsonResponse();
         $response ->setData([
             'succes' => true,
